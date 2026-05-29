c84293ac49f117858904da78ed59a58071e2f62f
haroon <haroon.naseer@bcbpsolutions.com>
2026-05-29 11:34:10 +0530

    feat(adapter): add gRPC server stub for DefinitionService

---
+++ internal/adapter/inbound/grpc/server.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	definitionv1 "github.com/BCBP-SOLUTIONS-FZC-LLC/workflow-definition-service/gen/proto/definition/v1"
+	"github.com/BCBP-SOLUTIONS-FZC-LLC/workflow-definition-service/internal/core/port"
+)
+
+type Server struct {
+	definitionv1.UnimplementedDefinitionServiceServer
+	log port.Logger
+}
+
+func NewServer(log port.Logger) *Server {
+	return &Server{log: log}
+}
+
+func (s *Server) GetCompiledWorkflow(
+	_ context.Context,
+	_ *definitionv1.GetCompiledWorkflowRequest,
+) (*definitionv1.GetCompiledWorkflowResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not yet implemented")
+}
