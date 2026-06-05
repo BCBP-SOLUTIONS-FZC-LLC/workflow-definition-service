addfb3f3f9ed4007615a96564a9fb55cec022be1
haroon <haroon.naseer@bcbpsolutions.com>
2026-06-05 13:43:27 +0530

    test: add unit test placeholder files for all stub endpoints

---
+++ test/unit/service/validation_service_test.go
@@ -0,0 +1,9 @@
+package service_test
+
+import (
+	"testing"
+)
+
+func TestValidationService_Validate(t *testing.T) {
+	t.Skip("TODO: implement when ValidationService.Validate business logic is wired")
+}
